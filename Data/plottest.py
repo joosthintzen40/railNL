8c05bf3b43a93c0c2e457c1ff66670514add8b8d
MinorDelay <simonkemmere1@gmail.com>
2018-05-26 00:08:49 +0200

    clean data

--- Data/plottest.py
+++
@@ -1,9 +0,0 @@
-import plotly
-
-import matplotlib.pyplot as plt
-import plotly.plotly as py
-from extract_coords import xcoord,ycoord
-
-matplotlib.pyplot.plot(xcoord, 'o')
-matplotlib.pyplot.plot(ycoord)
-plot.gcf()
